fe09b720127d3e80030ce78d3f80afef3028a594
Durdagi Lab <146360745+DurdagiLab@users.noreply.github.com>
2023-09-29 16:52:16 +0300

    Update split.sh

--- split.sh
+++ split.sh
@@ -2,7 +2,7 @@
 
 for z in structure_*.maegz;do
         st=${z%.maegz}
-        $SCHRODINGER18/run pv_convert.py -mode split_receptor $st.maegz -o structure-$st.maegz
-        $SCHRODINGER18/run pv_convert.py -mode split_ligand $st.maegz -o ligand-$st.maegz
+        $SCHRODINGER/run pv_convert.py -mode split_receptor $st.maegz -o structure-$st.maegz
+        $SCHRODINGER/run pv_convert.py -mode split_ligand $st.maegz -o ligand-$st.maegz
 done
 
