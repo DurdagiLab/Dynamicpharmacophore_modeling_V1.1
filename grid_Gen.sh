b0fcf90f172308dc36ae9118734d39d9e44a8204
Durdagi Lab <146360745+DurdagiLab@users.noreply.github.com>
2023-09-29 16:52:04 +0300

    Update grid_Gen.sh

--- grid_Gen.sh
+++ grid_Gen.sh
@@ -3,5 +3,5 @@ read -p "To generate grid of your Protein-Ligand complex i need to be sure about
 parent=$r1
 for i in *.maegz;do
         name=${i%.maegz}
-        $SCHRODINGER18/utilities/generate_glide_grids -rec_file $name.maegz -lig_asl "res.ptype $parent" -j $name -write_in
+        $SCHRODINGER/utilities/generate_glide_grids -rec_file $name.maegz -lig_asl "res.ptype $parent" -j $name -write_in
 done
