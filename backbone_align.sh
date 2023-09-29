063eb782c3bea7ad86c14b6b8246c289387edc47
Durdagi Lab <146360745+DurdagiLab@users.noreply.github.com>
2023-09-29 16:51:51 +0300

    Update backbone_align.sh

--- backbone_align.sh
+++ backbone_align.sh
@@ -6,6 +6,6 @@ name=$f
 for i in desmond_md*_trj;do
 trj=$i
 
-$SCHRODINGER18/run trj_align.py $name $trj ${trj}_aligned_backbone -s ::1 -asl '( backbone )' -ref-frame 0
+$SCHRODINGER/run trj_align.py $name $trj ${trj}_aligned_backbone -s ::1 -asl '( backbone )' -ref-frame 0
 done
 done
