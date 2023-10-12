3ed50731f51ab6c3666e4328d989fd9774495068
Durdagi Lab <146360745+DurdagiLab@users.noreply.github.com>
2023-10-12 16:04:21 +0300

    Update epharmacophore_analyze.sh

--- epharmacophore_analyze.sh
+++ epharmacophore_analyze.sh
@@ -2,7 +2,7 @@
 ./art.py
 read -p "Please Give the path of your MD output file : " -r r1
 parent=$r1
-cp ePharm.sh grid_Gen.sh quotes.sh split.sh art.py backbone_align.sh trjwrite.sh $parent/
+cp featureFromRMSD.py ePharm.sh grid_Gen.sh quotes.sh split.sh art.py backbone_align.sh trjwrite.sh $parent/
 cd $parent
 echo $parent
 #./art.py
@@ -12,7 +12,7 @@ echo "All your trajectories are write in individual files ... "
 ./trjwrite.sh
 mkdir TRJ/
 mv *.maegz TRJ/
-cp ePharm.sh grid_Gen.sh quotes.sh split.sh art.py TRJ/
+cp featureFromRMSD.py ePharm.sh grid_Gen.sh quotes.sh split.sh art.py TRJ/
 cd TRJ/
 ./quotes.sh
 ./grid_Gen.sh
